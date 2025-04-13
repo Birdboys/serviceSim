b6b65649721abfcd94d14b010dfe1aff9c6e6ddb
Birdboys <colbyjbird@comcast.net>
2025-04-13 16:14:26 -0600

    title screen is real

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,9 +1,74 @@
-extends Control
+extends Node3D
 
-@onready var bedMenu := preload('res://scenes/bed_menu.tscn')
-var playing := false
+@onready var roadTile := $roadLTile
+@onready var uiAnim := $titleUI/uiAnim
+@onready var settingsVbox := $titleUI/titleMargin/settingsVbox
+@onready var buttonVbox := $titleUI/titleMargin/buttonVbox
+@onready var creditsVbox := $titleUI/titleMargin/creditsVbox
+
+@onready var playButton := $titleUI/titleMargin/buttonVbox/playButton
+@onready var settingsButton := $titleUI/titleMargin/buttonVbox/settingsButton
+@onready var settingsBack := $titleUI/titleMargin/settingsVbox/backButton
+@onready var creditsButton := $titleUI/titleMargin/buttonVbox/creditsButton
+@onready var creditsBack := $titleUI/titleMargin/creditsVbox/backButton
+@onready var quitButton := $titleUI/titleMargin/buttonVbox/quitButton
+
+@onready var musicSlider := $titleUI/titleMargin/settingsVbox/mainVbox/musicSlider/musicSlider
+@onready var soundSlider := $titleUI/titleMargin/settingsVbox/mainVbox/soundSlider/soundSlider
+
+var current_menu := "main"
+
+func _ready() -> void:
+	
+	
+	playButton.pressed.connect(toBed)
+	settingsButton.pressed.connect(loadMenu.bind("settings"))
+	settingsBack.pressed.connect(loadMenu.bind("main"))
+	creditsButton.pressed.connect(loadMenu.bind("credits"))
+	creditsBack.pressed.connect(loadMenu.bind("main"))
+	quitButton.pressed.connect(get_tree().quit)
+	loadTitle()
+	
+func loadTitle():
+	uiAnim.play("RESET")
+	loadMenu("main")
+	await roadTile.initTrash()
+	uiAnim.play("load_menu")
+	await uiAnim.animation_finished
+	uiAnim.play("idle_menu")
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey and event.keycode != KEY_ESCAPE and event.keycode != KEY_ALT and event.keycode != KEY_TAB and not playing:
-		playing = true
-		get_tree().change_scene_to_packed(bedMenu)
+	if event.is_action_pressed("escape"): loadMenu("main")
+	#elif event is InputEventKey and event.keycode == KEY_F5: loadTitle()
+	
+func loadMenu(m: String):
+	current_menu = m
+	match m:
+		"main":
+			buttonVbox.visible = true
+			settingsVbox.visible = false
+			creditsVbox.visible = false
+		"settings":
+			setSliders()
+			settingsVbox.visible = true
+			buttonVbox.visible = false
+		"credits":
+			creditsVbox.visible = true
+			buttonVbox.visible = false
+	
+func updateMusic(v):
+	GameData.settings_data['music'] = v/100.0
+	AudioHandler.loadAudioSettings()
+	
+func updateSound(v):
+	GameData.settings_data['sound'] = v/100.0
+	AudioHandler.loadAudioSettings()
+
+func setSliders():
+	musicSlider.value = GameData.settings_data['music'] * 100.0
+	soundSlider.value = GameData.settings_data['sound'] * 100.0
+
+func toBed():
+	uiAnim.play("to_bed")
+	await uiAnim.animation_finished
+	get_tree().change_scene_to_file("res://scenes/bed_menu.tscn")
